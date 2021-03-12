6a56277a67fea28d3341bb43631a5ae3f0b1404b
tetuyasan <nukunukuohuton@gmail.com>
2021-03-12 18:26:55 +0900

    エラ〜メッセージの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ApplicationRecord
   has_many :buys
 
   validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :family_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :birthday, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
