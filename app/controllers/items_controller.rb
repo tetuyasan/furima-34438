9654a977b7e70414d8b971b10aa735f9e6f90b28
tetuyasan <nukunukuohuton@gmail.com>
2021-03-17 18:17:43 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show,]
+  before_action :set_items,only: [:edit,:show,:update]
+  before_action :move_to_root_path, only: [:edit]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -13,14 +15,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  #def destroy
-  #   @items = Item.find(params[:id])
-  #   items.destroy
-  # end
+        #def destroy
+        #   @items = Item.find(params[:id])
+        #   items.destroy
+        # end
 
-  # def edit
-  #   @items = Item.find(params[:id])
-  # end
+  def edit
+     @items = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def create
     @item = Item.new(item_params)
@@ -32,6 +44,16 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_items
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_root_path
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
 
   def item_params
     params.require(:item).permit(:comment, :image, :item_name, :category_id, :condition_id, :shipping_charge_id, :area_id, :shipping_id, :item_price).merge(user_id: current_user.id)
