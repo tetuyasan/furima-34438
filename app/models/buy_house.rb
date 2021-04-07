8559b213712224031788caf09734b3feacc593bd
tetuyasan <nukunukuohuton@gmail.com>
2021-04-07 18:39:58 +0900

    修正

--- app/models/buy_house.rb
+++ app/models/buy_house.rb
@@ -9,9 +9,10 @@ class BuyHouse
     validates :user_id
     validates :item_id
     validates :phone_number, length: { maximum: 11}
-    validates :phone_number, numericality:true
     validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
   end
+
+    validates :phone_number, numericality:true
 
   with_options numericality: { other_than: 1 } do
     validates :area_id
